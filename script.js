6db488da35423ecced5cd8697f3dd20714b3683a
Manisha Mali <82174253+M74-dot@users.noreply.github.com>
2024-08-01 22:23:32 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,7 +1,12 @@
 function findLargest(a, b, c) {
   //your code here
-	return Math.max(a,b,c);
+	// return Math.max(a,b,c);
+	if(a>b && a>c)return a;
+	else if(b>a && b>c)return b;
+	else if(c>a && c>b)return c;
 }
+
+
 
 const num1 = parseInt(prompt("Enter First Number."));
 const num2 = parseInt(prompt("Enter Second Number."));
